7641312c3fdbbfd7cc8ad013f82192b6cb9015f0
Jessi Zimmerman <112185795+jessi-z3@users.noreply.github.com>
2023-06-07 16:57:00 -0500

    Initial Commit

---
+++ Reese-ources/Reese_ourcesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reese_ourcesApp.swift
+//  Reese-ources
+//
+//  Created by Jessi on 6/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Reese_ourcesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
